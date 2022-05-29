852b3ff17132573c9a3b959317358bcc2a900fc7
Peter Klingelhofer <peterklingelhofer@gmail.com>
2022-05-28 23:53:37 -0500

    Import external Readmes, remove blog section

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,8 +9,8 @@ const config = {
   title: 'Resonate Documentation',
   tagline: 'Documentation for the tech behind the co-operative music streaming service',
   url: 'https://resonate.coop',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
+  baseUrl: '/docs/',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
@@ -18,6 +18,9 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'resonatecoop', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
+  deploymentBranch: 'gh-pages', // GitHub Pages requires this to be explicit as a config field or environment variable.
+  trailingSlash: false, // GitHub Pages adds a trailing slash to Docusaurus URLs by default.
+                        // It is recommended to set a trailingSlash config(true or false, not undefined).
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -27,6 +30,177 @@ const config = {
     locales: ['en'],
   },
 
+  plugins: [
+    [
+        "docusaurus-plugin-remote-content",
+        {
+          name: "stream",
+          sourceBaseUrl: "https://raw.githubusercontent.com/resonatecoop/stream/development/",
+          outDir: "docs/basics",
+          documents: ["README.md"],
+          modifyContent(filename, content) {
+            if (filename.includes("README")) {
+              return {
+                filename: "stream.md",
+                content: `---
+sidebar_position: 1
+---
+
+Explore the monorepo for the web player, the <code>[stream](https://github.com/resonatecoop/stream)</code> repository...
+
+${content.replace('<!-- Standard -->', '').replace('stream2own', '')}`,
+              }
+            }
+            return undefined
+          },
+        }
+    ],
+    [
+        "docusaurus-plugin-remote-content",
+        {
+          name: "website",
+          sourceBaseUrl: "https://raw.githubusercontent.com/resonatecoop/website/develop/",
+          outDir: "docs/basics",
+          documents: ["README.md"],
+          modifyContent(filename, content) {
+            if (filename.includes("README")) {
+              return {
+                filename: "website.md",
+                content: `---
+sidebar_position: 2
+---
+
+Explore the Resonate <code>[website](https://github.com/resonatecoop/website)</code> repository...
+
+${content}`,
+              }
+            }
+            return undefined
+          },
+        },
+    ],
+    [
+        "docusaurus-plugin-remote-content",
+        {
+          name: "id",
+          sourceBaseUrl: "https://raw.githubusercontent.com/resonatecoop/id/develop/",
+          outDir: "docs/basics",
+          documents: ["README.md"],
+          modifyContent(filename, content) {
+            if (filename.includes("README")) {
+              return {
+                filename: "id.md",
+                content: `---
+sidebar_position: 3
+---
+
+Explore the <code>[id](https://github.com/resonatecoop/id)</code> server repository...
+
+${content}`,
+              }
+            }
+            return undefined
+          },
+        },
+    ],
+    [
+        "docusaurus-plugin-remote-content",
+        {
+          name: "user-api",
+          sourceBaseUrl: "https://raw.githubusercontent.com/resonatecoop/user-api/master/",
+          outDir: "docs/basics",
+          documents: ["README.md"],
+          modifyContent(filename, content) {
+            if (filename.includes("README")) {
+              return {
+                  filename: "user-api.md",
+                  content: `---
+sidebar_position: 4
+---
+
+Explore the Golang-based <code>[user-api](https://github.com/resonatecoop/user-api)</code> repository...
+
+${content}`,
+              }
+            }
+            return undefined
+          },
+        }
+    ],
+    [
+        "docusaurus-plugin-remote-content",
+        {
+          name: "user-api-client",
+          sourceBaseUrl: "https://raw.githubusercontent.com/resonatecoop/user-api-client/master/",
+          outDir: "docs/basics",
+          documents: ["README.md"],
+          modifyContent(filename, content) {
+            if (filename.includes("README")) {
+              return {
+                  filename: "user-api-client.md",
+                  content: `---
+sidebar_position: 5
+---
+
+Explore the goswagger.io generated client for <code>user-api</code>, the <code>[user-api-client](https://github.com/resonatecoop/user-api-client)</code> repository...
+
+${content}`,
+              }
+            }
+            return undefined
+          },
+        },
+    ],
+    [
+        "docusaurus-plugin-remote-content",
+        {
+          name: "beam",
+          sourceBaseUrl: "https://raw.githubusercontent.com/simonv3/beam/main/",
+          outDir: "docs/ecosystem",
+          documents: ["README.md"],
+          modifyContent(filename, content) {
+            if (filename.includes("README")) {
+              return {
+                  filename: "beam.md",
+                  content: `---
+sidebar_position: 1
+---
+
+Explore the <code>[beam](https://github.com/simonv3/beam)</code> desktop app repository...
+
+${content}`,
+              }
+            }
+            return undefined
+          },
+        },
+    ],
+    [
+        "docusaurus-plugin-remote-content",
+        {
+          name: "stream-app",
+          sourceBaseUrl: "https://raw.githubusercontent.com/peterklingelhofer/stream-app/main/",
+          outDir: "docs/ecosystem",
+          documents: ["README.md"],
+          modifyContent(filename, content) {
+            if (filename.includes("README")) {
+              return {
+                filename: "stream-app.md",
+                content: `---
+sidebar_position: 2
+---
+
+Explore the mobile & tablet <code>[stream-app](https://github.com/peterklingelhofer/stream-app)</code> repository...
+
+${content}`,
+              }
+            }
+            return undefined
+          },
+        },
+      ],
+  ],
+  
   presets: [
     [
       'classic',
@@ -36,15 +210,13 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/resonatecoop/docs/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl: 'https://github.com/resonatecoop/docs/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/resonatecoop/docs/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl: 'https://github.com/resonatecoop/docs/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
