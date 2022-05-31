78a9aec04e09329e173163e21cd8600688ac918c
auggod <dev@auggod.io>
2022-05-31 19:36:50 +0200

    Revert "Remove base url"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Resonate Documentation',
   tagline: 'Documentation for the tech behind the co-operative music streaming service',
   url: 'https://resonate.coop',
-  baseUrl: '/',
+  baseUrl: '/documentation/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
