c11a665ab61113f0b58387d33ab300ba1441498a
Augustin Godiscal <hello@auggod.tech>
2022-05-31 19:59:12 +0200

    Update link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/documentation/docs/intro">
+            to="/docs/intro">
             Getting Started
           </Link>
         </div>
